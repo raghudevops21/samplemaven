7d45246cea5da0457e6c3d312d0f7b404ba2ae14
raghunath <raghudevops21@gmail.com>
2018-10-13 12:23:17 +0530

    maven build

---
+++ src/main/java/com/samplemaven/raghu.java
@@ -0,0 +1,7 @@
+public class raghu {
+
+  public static void main (String[] args) {
+
+	System.out.println ("Hi Raghunath, this is your first jenkins job at Techchefs");
+}
+}
---
+++ src/test/java/com/samplemaven/raghuTest.java
@@ -0,0 +1,7 @@
+public class raghuTest {
+
+  public static void main (String[] args) {
+
+	System.out.println ("Hi Raghunath, this is your first jenkins job at Techchefs");
+}
+}
